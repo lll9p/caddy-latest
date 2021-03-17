cf1a8100b82424be0ec022ab9cfe9e1fdcd86097
Lilin Lao <lll9p.china@gmail.com>
2021-03-17 13:52:22 +0800

    init

---
+++ build.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+export PATH=$PATH:$PWD/go/bin
+go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+xcaddy build \
+    --output ./
+    --with github.com/mholt/caddy-webdav \
+    --with github.com/caddy-dns/dnspod \
+    --with github.com/caddy-dns/cloudflare
+ls -al ./
